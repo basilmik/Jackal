18fbffcb025b0e47a8662a2fba4fba3e29c5aecb
basil <basil@BASILIK>
2022-07-17 11:42:43 +0300

    add tile.kt

---
+++ app/src/main/java/com/basilgames/android/jackal/Tile.kt
@@ -0,0 +1,11 @@
+package com.basilgames.android.jackal
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity
+data class Tile (@PrimaryKey val id: UUID = UUID.randomUUID(),
+                 var isFaceUp: Boolean = false,
+                 var imageRes: Int = 0
+)
