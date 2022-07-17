b35e4f61fc8873ae30567b45a5c8e9c56ab6773e
basil <basil@BASILIK>
2022-07-17 11:47:10 +0300

    definition of db and tables

---
+++ app/src/main/java/com/basilgames/android/jackal/database/TileDatabase.kt
@@ -0,0 +1,11 @@
+package com.basilgames.android.jackal.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.basilgames.android.jackal.Tile
+
+@Database(entities = [ Tile::class ], version = 1)
+@TypeConverters(TileTypeConverters::class)
+abstract class TileDatabase: RoomDatabase() {
+}
